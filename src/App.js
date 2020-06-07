549909bd69a9235d5f4ea737e02f49c2ca9f86b4
unknown <mnkcje2050@gmail.com>
2020-06-07 15:31:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/*https://stackoverflow.com/questions/41337709/what-is-use-of-curly-braces-in-es6-import-statement/41338672*/
+import Amplify, {Analytics, AWSKinesisFirehoseProvider} from 'aws-amplify';
+import config from './aws-exports';
 
+Amplify.configure({
+
+  Auth:{
+    identityPoolId: config.aws_cognito_identity_pool_id,
+    region:config.aws_project_region
+  },
+  Analytics:{
+    AWSKinesisFirehose:{
+      region: config.aws_project_region
+    }
+  }
+});
+
+Analytics.addPluggable(new AWSKinesisFirehoseProvider());
+
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			languages : [
+				{name: "Php", votes: 0},
+				{name: "Python", votes: 0},
+				{name: "Go", votes: 0},
+				{name: "Java", votes: 0}
+			]
+		}
+	}
+
+	vote (i) {
+		let newLanguages = [...this.state.languages];
+		newLanguages[i].votes++;
+		function swap(array, i, j) {
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+    this.setState({languages: newLanguages});
+    
+    //Analytics Integration here : Pushing the Votes to Analytics
+    const now = new Date();
+    let data = {
+      id:now.getTime(),
+	  language:newLanguages[i].name,
+	  PartitionKey:now.getTime()
+	}    
+	console.log('Put Record');
+    Analytics.record({
+      data:data,
+      streamName:config.aws_firehose_name
+    ,},'AWSKinesisFirehose');    
+	}
+
+	render(){
+		return(
+			<>
+				<h1>Vote Your Language!</h1>
+				<div className="languages">
+					{
+						this.state.languages.map((lang, i) => 
+							<div key={i} className="language">
+								<div className="voteCount">
+									{lang.votes}
+								</div>
+								<div className="languageName">
+									{lang.name}
+								</div>
+								<button onClick={this.vote.bind(this, i)}>Click Here</button>
+							</div>
+						)
+					}
+				</div>
+			</>
+		);
+	}
+}
 export default App;
